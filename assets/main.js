4894fd7599ca57c6601d3caf85870a219bced0c7
yutianerme <wuzhenxingbupt@163.com>
2019-03-31 14:54:12 +0800

    add mobile events

--- assets/main.js
+++ assets/main.js
@@ -2,69 +2,16 @@ $(document).ready(function() {
 
   'use strict';
 
-  // SOUNDCLOUD PLAYER
-  // ================================
-  var musicFrame = $('.music-frame');
-  var musicButton = $('.music-button');
-  musicFrame.hide();
-  musicButton.click(function(){
-    musicFrame.toggle();
-  });
-
-  $(window).on('scroll', function() {
-    var wScroll = $(window).scrollTop();
-    if (wScroll >= 800) {
-      musicButton.css('right', '30px');
-      musicFrame.css('right', '60px');
-    } else if (wScroll < 800) {
-      musicButton.css('right', '-50px');
-      musicFrame.css('right', '-300px').hide();
-    }
-  });
+  $("#my-world").on("touchstart", touchStart);
+  $("#my-world").on("touchend", touchEnd);
 
 
-  // NAVBAR SMOOTH SCROLLING
-  // ================================
-  $('.navbar-nav').localScroll();
+ 
+  function touchStart(event) {
+    document.getElementById("my-world").innerText="World";
+  }
 
-
-  // MAGNIFIC POPUP FOR BRIDE GALLERY
-  // ================================
-  $('.bride-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    gallery: {
-      enabled: true
-    },
-    mainClass: 'mfp-with-zoom',
-    zoom: {
-      enabled: true,
-      duration: 300,
-      easing: 'ease-in-out',
-      opener: function(openerElement) {
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-      }
-    }
-  });
-
-
-  // MAGNIFIC POPUP FOR GROOM GALLERY
-  // ==============================
-  $('.groom-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    gallery: {
-      enabled: true
-    },
-    mainClass: 'mfp-with-zoom',
-    zoom: {
-      enabled: true,
-      duration: 300,
-      easing: 'ease-in-out',
-      opener: function(openerElement) {
-        return openerElement.is('img') ? openerElement : openerElement.find('img');
-      }
-    }
-  });
-
+  function touchEnd(event) {
+    document.getElementById("my-world").innerText="添儿";
+  }
 });
